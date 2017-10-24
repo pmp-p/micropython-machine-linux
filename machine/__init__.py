cf6e3eb949a27e42ef41f9ce847cb5d8314f09fd
pmp-p <pmpp.pub@gmail.com>
2017-10-24 09:02:39 +0200

    upy compat

--- machine/__init__.py
+++ machine/__init__.py
@@ -3,9 +3,24 @@ __version__ = "1.1.0"
 
 version = (1,1,0)
 "Module version tuple."
+import sys
 
-import time
+import mpycompat
 
+if 0:
+    try:
+        import time
+    except ImportError:
+        import utime as time
+        builtins.time =time
+       
+#        import uos as os
+#        sys.modules['os']=os
+        
+        #builtins.IOError = OSError
+        
+    
+    
 def sleep(seconds):
     """Sleep for the specified number of seconds.
 
@@ -34,10 +49,101 @@ def sleep_us(microseconds):
     time.sleep(microseconds / 1000000.0)
 
 from machine.gpio import GPIO, GPIOError
-from machine.led import LED, LEDError
-from machine.pwm import PWM, PWMError
+try:
+    from machine.led import LED, LEDError
+except ImportError as e:
+    print("machine.led could not import :", e, file=sys.stderr)
+try:
+    from machine.pwm import PWM, PWMError
+except ImportError as e:
+    print("machine.pwm could not import :", e, file=sys.stderr)
+        
 from machine.spi import SPI
-from machine.i2c import I2C, I2CError
-from machine.mmio import MMIO, MMIOError
+
+try:
+    from machine.i2c import I2C, I2CError
+except ImportError as e:
+    print("machine.pwm could not import :", e, file=sys.stderr)
+
+try:
+    from machine.mmio import MMIO, MMIOError
+except ImportError as e:
+    print("machine.mmio could not import :", e, file=sys.stderr)
+    
 from machine.serial import Serial, SerialError
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def SUNXI_GPIO(pbanknum):
+    pbanknum = pbanknum.upper()
+    bank = ( ord(pbanknum[1]) -65 ) * 32
+    pin = int(pbanknum[2:])
+    print(pbanknum,bank,pin)
+    return bank + pin
+
+
+
+class Pin(GPIO):
+    IN = 'r'
+    OUT = 'w'
+    PULL_UP = 'h'
+    PULL_DOWN = None
+
+    def __init__(self,pbanknum, mode=None, pull=None):
+        self.name = pbanknum
+        self.gpio = SUNXI_GPIO(self.name)
+        pull = pull or ''
+        mode = mode or ''
+
+        self.pull = pull.lower()
+        self.mode = mode.lower()
+        if self.pull or self.mode:
+            self.init(self.mode, self.pull)
+
+
+    def init(self,mode='',pull='l',**kw):
+        if 'w' in mode:
+            direction = 'out'
+        elif 'r' in mode:
+            direction = 'in'
+            if pull:
+                if 'h' in pull:
+                    direction = 'high'
+                elif 'l' in pull:
+                    direction = 'low'
+        else:
+            direction = 'preserve'
+        GPIO.__init__(self, self.gpio, direction)
+
+        self.value( kw.pop('value',False) )
+
+
+    def value(self,v=None):
+        if v is None:
+            return self.read()
+        self.write( (v and True) or False)
+
+    def off(self):
+        self.write(False)
+
+    def on(self):
+        self.write(True)
+
+    def __repr__(self):
+        return "(%s)%s"%(self.gpio, self.name)
+
 
